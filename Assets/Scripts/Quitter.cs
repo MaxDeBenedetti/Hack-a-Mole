656a6328fb4ace1249068ccbfbabfa61ac18ff7d
MaxDeBenedetti <modsme@gmail.com>
2016-12-18 15:25:50 -0600

    added a quitter

---
+++ Assets/Scripts/Quitter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quitter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            LeaveGame();
+        }
+	}
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+}
